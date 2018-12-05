947f59cc5727e2468c91a312734a62bb6848e07e
Daniel Toma <danieltoma@ubalumni.com>
2018-12-05 05:37:11 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    counter = 0
+    new_collection = []
+    while counter < collection.length
+      if yield(collection[counter])
+        new_collection << collection[counter]
+      end
+      counter += 1
+    end
+    new_collection
+  end
 end
